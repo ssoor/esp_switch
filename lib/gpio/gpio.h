35695e4e4c1e217a995b73c7c7c69d27b43e74a2
ssoor <ssoor@mail.com>
2019-07-24 09:28:54 +0800

    feature: 添加 GPIO 相关宏定义

---
+++ lib/gpio/gpio.h
@@ -0,0 +1,53 @@
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+#include <esp8266/gpio_register.h>
+
+#define GPIO0 0
+#define GPIO1 1
+#define GPIO2 2
+#define GPIO3 3
+#define GPIO4 4
+#define GPIO5 5
+#define GPIO6 6
+#define GPIO7 7
+#define GPIO8 8
+#define GPIO9 9
+#define GPIO10 10
+#define GPIO11 11
+#define GPIO12 12
+#define GPIO13 13
+#define GPIO14 14
+#define GPIO15 15
+
+#define GPIO_PIN_REG_0 PERIPHS_IO_MUX_GPIO0_U
+#define GPIO_PIN_REG_1 PERIPHS_IO_MUX_U0TXD_U
+#define GPIO_PIN_REG_2 PERIPHS_IO_MUX_GPIO2_U
+#define GPIO_PIN_REG_3 PERIPHS_IO_MUX_U0RXD_U
+#define GPIO_PIN_REG_4 PERIPHS_IO_MUX_GPIO4_U
+#define GPIO_PIN_REG_5 PERIPHS_IO_MUX_GPIO5_U
+#define GPIO_PIN_REG_6 PERIPHS_IO_MUX_SD_CLK_U
+#define GPIO_PIN_REG_7 PERIPHS_IO_MUX_SD_DATA0_U
+#define GPIO_PIN_REG_8 PERIPHS_IO_MUX_SD_DATA1_U
+#define GPIO_PIN_REG_9 PERIPHS_IO_MUX_SD_DATA2_U
+#define GPIO_PIN_REG_10 PERIPHS_IO_MUX_SD_DATA3_U
+#define GPIO_PIN_REG_11 PERIPHS_IO_MUX_SD_CMD_U
+#define GPIO_PIN_REG_12 PERIPHS_IO_MUX_MTDI_U
+#define GPIO_PIN_REG_13 PERIPHS_IO_MUX_MTCK_U
+#define GPIO_PIN_REG_14 PERIPHS_IO_MUX_MTMS_U
+#define GPIO_PIN_REG_15 PERIPHS_IO_MUX_MTDO_U
+
+#define GPIO_FUNC(no) FUNC_GPIO##no
+#define GPIO_PIN_REG(no) GPIO_PIN_REG_##no
+
+#define GPIO_ENABLE(no) \
+    PIN_FUNC_SELECT(GPIO_PIN_REG(no), GPIO_FUNC(no))
+
+#define GPIO_OUTPUT_SET(no, value)                                                            \
+    GPIO_REG_WRITE(((value & 0x1) ? GPIO_OUT_W1TS_ADDRESS : GPIO_OUT_W1TC_ADDRESS), BIT(no)); \
+    GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, BIT(no));
+
+#define GPIO_OUTPUT_GET(no, value) \
+    GPIO_REG_READ(GPIO_IN_ADDRESS);
+
+#endif
